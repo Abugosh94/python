0cde97f8a5880183710c9612afd2948b79f912ff
Asad <abugosh94@gmail.com>
2023-05-04 14:18:26 +0300

    Comments commit

--- _python/python_fundementals/for_loop_basic_1.py
+++ _python/python_fundementals/for_loop_basic_1.py
@@ -1,25 +1,31 @@
+#Basics
 for x in range(1, 151, 1):
     print(x)
 
+#Multiples of Five
 for x in range(5, 1001, 1):
     if(x%5==0):
         print(x)
 
+#Counting, the Dojo Way
 for x in range(1, 101, 1):
     if(x%10==0):
         print("Coding Dojo")
     elif (x%5==0):
         print("Coding")
 
+#Whoa. That Sucker's Huge
 sum=0
 for x in range(0, 500001, 1):
     if(x%2!=0):
         sum+=x
 print(sum)
 
+#Countdown by Fours
 for x in range(2018, 0, -4):
     print(x)
 
+#Flexible Counter
 low_num = 2
 high_num = 54
 mult= 5
