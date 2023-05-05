712adc3acd53d14123eb26d6fed4abbcfe43cf96
Asad <abugosh94@gmail.com>
2023-05-05 13:13:52 +0300

    Edit

--- _python/python_fundementals/for_loop_basic_1.py
+++ _python/python_fundementals/for_loop_basic_1.py
@@ -1,11 +1,10 @@
 #Basics
-for x in range(1, 151, 1):
+for x in range(0, 151, 1):
     print(x)
 
 #Multiples of Five
-for x in range(5, 1001, 1):
-    if(x%5==0):
-        print(x)
+for x in range(5, 1001, 5):
+    print(x)
 
 #Counting, the Dojo Way
 for x in range(1, 101, 1):
@@ -13,10 +12,12 @@ for x in range(1, 101, 1):
         print("Coding Dojo")
     elif (x%5==0):
         print("Coding")
+    else:
+        print(x)
 
 #Whoa. That Sucker's Huge
 sum=0
-for x in range(0, 500001, 1):
+for x in range(0, 500001):
     if(x%2!=0):
         sum+=x
 print(sum)
