b324f419359ffd9d5d9184911a0693c51dd09385
Asad <abugosh94@gmail.com>
2023-05-16 15:53:15 +0300

    Hello Flask

--- my_environments/py3Env/Scripts/activate.bat
+++ my_environments/py3Env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asad\Desktop\A\Ta5abees\python_stack\my_environments\py3env
+set VIRTUAL_ENV=C:\Users\Asad\Desktop\A\Ta5abees\python_stack\my_environments\py3Env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(py3env) %PROMPT%
+set PROMPT=(py3Env) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(py3env) 
+set VIRTUAL_ENV_PROMPT=(py3Env) 
 
 :END
 if defined _OLD_CODEPAGE (
